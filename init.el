fa254f0ebb5c1b426a86ffe8b0ccad3d967994fc
simmhugs <simon.graetz@gmx.de>
2024-10-19 11:24:00 +0200

    updated windows init

--- init.el
+++ init.el
@@ -1,18 +1,22 @@
 (package-initialize)
 (cond ((string= system-type "gnu/linux")
        (progn
+         ;; linux
 	 (load-file "~/.config/emacs/config/new-basics.el")
 	 (load-file "~/.config/emacs/config/straight-init.el")
 	 (load-file "~/.config/emacs/config/packages/orgmode.el")
 	 (load-file "~/.config/emacs/config/packages/packages.el")
 	 (load-file "~/.config/emacs/config/packages/lsp.el")
-	 (load-file "~/.config/emacs/config/new-theme.el")))
+	 (load-file "~/.config/emacs/config/new-theme.el")
+         ))
       (t
        (progn
+         ;; windows
+	 (load-file "~/.emacs.d/config/new-basics.el")
 	 (load-file "~/.emacs.d/config/straight-init.el")
 	 (load-file "~/.emacs.d/config/packages/orgmode.el")
 	 (load-file "~/.emacs.d/config/packages/packages.el")
 	 (load-file "~/.emacs.d/config/packages/lsp.el")
 	 (load-file "~/.emacs.d/config/new-theme.el")
-	 (load-file "~/.emacs.d/config/new-basics.el"))))
+         )))
 
